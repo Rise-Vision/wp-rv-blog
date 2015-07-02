e853eb7df9aff28c515b2d2775306f2f225b3816
RyanPet <buhatsatambay@gmail.com>
2015-07-03 06:29:32 +0800

    updated favicon

--- header.php
+++ header.php
@@ -7,7 +7,8 @@
     <title><?php wp_title( '|', true, 'right' ); ?></title>
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.ico" type="image/x-icon">
     
     <?php wp_head(); ?>
 
