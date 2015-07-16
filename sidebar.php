b9d334961ce00f91790d2964b422dc35c6c2ebb9
RyanPet <buhatsatambay@gmail.com>
2015-07-16 09:56:42 +0800

    updated mailchimp form

--- sidebar.php
+++ sidebar.php
@@ -27,8 +27,11 @@
 						<div id="mce-responses" class="clear">
 							<div class="response" id="mce-error-response" style="display:none"></div>
 							<div class="response" id="mce-success-response" style="display:none"></div>
-						</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-						<div style="position: absolute; left: -5000px;"><input type="text" name="b_5074ef419fc3c9f4a4737962e_c0286621a0" tabindex="-1" value=""></div>
+						</div>    
+						<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+						<div style="position: absolute; left: -5000px;">
+							<input type="text" name="b_5074ef419fc3c9f4a4737962e_c0286621a0" tabindex="-1" value="">
+						</div>
 					</div>
 				</form>
 			</div>
