6f28240cde4d5fb533494257b2b0cc9ccd30225e
RyanPet <buhatsatambay@gmail.com>
2015-07-18 00:20:20 +0800

    removed angular on sidebar

---
+++ sidebar.php
@@ -0,0 +1,8 @@
+<aside class="col-sm-5 col-lg-4 aside-section">
+	<div class="visible-xs">
+		<?php get_template_part( '_partials/content', 'box' ); ?>
+	</div>
+	<div class="hidden-xs" id="sticker">
+		<?php get_template_part( '_partials/content', 'box' ); ?>
+	</div>
+</aside>
